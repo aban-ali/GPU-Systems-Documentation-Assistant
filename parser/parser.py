dc845b13528c7b7f28876089549d89963488071f
aban-ali <22f1001086@ds.study.iitm.ac.in>
2026-06-20 11:33:39 +0000

    Rectified minor logical errors

--- parser/parser.py
+++ parser/parser.py
@@ -9,6 +9,11 @@ RAW_OUTPUT_FILEPATH = path.join("data", "processed", "raw_data.jsonl")
 PATTERN_END = re.compile(r"#+\s(\*\*)?[\d\.\s]+Notice(\*\*)?")
 PATTERN_INVALID_LINE1 = re.compile(r"\(([a-zA-Z]+\s){3}[a-zA-Z]+\)")
 PATTERN_INVALID_LINE2 = re.compile(r"\*\*^[a-zA-Z].*\*\*")
+PATTERN_IMAGE = re.compile(r"(\*\*)?==>.*<==(\*\*)?")
+PATTERN_TABLE_JUNK = re.compile(r"-?<br>")
+PATTERN_ALPHA = re.compile(r"_?\\\\u03b1_?") # alpha character
+PATTERN_BETA = re.compile(r"_?\\\\u03b2_?") # beta character
+PATTERN_FORWARD_SLASH = re.compile(r"_?(\\\\u2215)+_?") # forward slash character
 
 # # This is supposed to be used later
 # files = listdir(path("data", "pdf_data"))
@@ -28,6 +33,14 @@ def is_invalid_line(line):
 def is_new_chapter(line):
     return re.fullmatch(r"#+\s(\*\*)?Chapter\s.+(\*\*)?", line)
 
+def clean_line(line):
+    line = PATTERN_IMAGE.sub("==>[ IMAGE ]<==", line)
+    line = PATTERN_TABLE_JUNK.sub("", line)
+    line = PATTERN_ALPHA.sub("α", line)
+    line = PATTERN_BETA.sub("β", line)
+    line = PATTERN_FORWARD_SLASH.sub("/", line)
+    return line.strip()
+
 def smarter_parse():
     with open(RAW_OUTPUT_FILEPATH, "w") as f:
         for file in files:
@@ -40,7 +53,8 @@ def smarter_parse():
             pre = True
 
             for line in doc:
-                if pre and not re.match(r"## (\*\*)?Chapter 1(\*\*)?", line.strip()):
+                line = line.strip()
+                if pre and not re.match(r"## (\*\*)?Chapter 1(\*\*)?", line):
                     continue
                 if PATTERN_END.fullmatch(line):
                     text = ""
@@ -59,7 +73,9 @@ def smarter_parse():
                     text = ""
                     continue
 
-                text += "\n\n" + line.strip()
+                line = clean_line(line)
+                if line:
+                    text += "\n\n" + line
 
 def simple_parse():
     with open(RAW_OUTPUT_FILEPATH, "w") as f:
